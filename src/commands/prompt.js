1e95743a4e957fd189ce5f9f6f147f3eb250fa56
maxim.shnyagin <maxim.shnyagin@latoken.com>
2023-05-27 20:53:43 +0300

    prompt command

---
+++ src/commands/prompt.js
@@ -0,0 +1,43 @@
+"use strict";
+const {
+    SlashCommandBuilder,
+    PermissionFlagsBits
+} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("create-completion")
+        .setDescription("Sends completion to GPT")
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addStringOption(option =>
+            option
+                .setName("prompt")
+                .setDescription("Prompt")
+                .setRequired(true)),
+
+    async execute(interaction) {
+        await interaction.deferReply({ ephemeral: false });
+
+        const input = interaction.options.getString("prompt");
+
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo", messages: [{ role: "assistant", content: normalizePrompt(input) }], stream: false
+            })
+        }).then(resp => resp.json());
+
+        console.log(response.choices[0].message.content);
+
+        await interaction.followUp(response.choices[0].message.content);
+    }
+};
+
+const normalizePrompt = (prompt) => {
+    return `Execute the instruction enclosed in <>, the answer must not be longer than 1800 characters.
+    <${prompt}>`;
+}
