3e80fb7a085603edd0a3b5b176022609c4346e34
envyvox <kkxo.mail@gmail.com>
2023-06-09 14:15:59 +0300

    Fix online members

--- src/features/member-counter.js
+++ src/features/member-counter.js
@@ -7,7 +7,7 @@ const { Client, ActivityType } = require("discord.js");
 async function updateMembersCounter(client) {
     const guild = await client.guilds.fetch(process.env.SERVER_ID);
     const guildMembers = await guild.members.fetch({ force: true });
-    const onlineMembers = guildMembers.filter(member => member.presence !== null && member.presence.status === "offline");
+    const onlineMembers = guildMembers.filter(member => member.presence !== null && member.presence.status !== "offline");
 
     client.user.setActivity(
         `на ${guildMembers.size} пользователей, ${onlineMembers.size} из них онлайн`,
